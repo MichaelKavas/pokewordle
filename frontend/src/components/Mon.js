3bed1f18a72be1fd2f4f5fd0e0f43a7054181a41
MichaelKavas <michaelk0106@gmail.com>
2024-08-05 22:21:55 -0500

    Stop the form from creating empty new components when a guess doesn't match a name or number in the list of Pokemon.

--- frontend/src/components/Mon.js
+++ frontend/src/components/Mon.js
@@ -20,26 +20,29 @@ function Mon({ id = 1 }) {
 
   // console.log(pokemon);
   
-  return (
-    <div class="grid mx-auto w-1/2">
-      <div class="flex p-2 gap-4 text-lg">
-        <div class="flex justify-center items-center w-1/6 bg-gray-400 h-12 rounded-lg">Name</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-500 h-12 rounded-lg">Image</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-400 h-12 rounded-lg">Type 1</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-500 h-12 rounded-lg">Type 2</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-400 h-12 rounded-lg">Height</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-500 h-12 rounded-lg">Weight</div>
+  if (pokemon.name) {
+    return (
+      <div class="grid mx-auto w-1/2">
+        <div class="flex p-2 gap-4 text-lg">
+          <div class="flex justify-center items-center w-1/6 bg-gray-400 h-12 rounded-lg">Name</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-500 h-12 rounded-lg">Image</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-400 h-12 rounded-lg">Type 1</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-500 h-12 rounded-lg">Type 2</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-400 h-12 rounded-lg">Height</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-500 h-12 rounded-lg">Weight</div>
+        </div>
+        <div class="flex p-2 gap-4 text-lg">
+          <div class="flex justify-center items-center w-1/6 bg-gray-400 h-24 rounded-lg">{pokemon.name}</div>
+          <img class="flex justify-center items-center w-1/6 bg-gray-500 h-24 object-cover rounded-lg" src={pokemon.image} alt=""></img>
+          <div class="flex justify-center items-center w-1/6 bg-gray-400 h-24 rounded-lg">{pokemon.type1}</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-500 h-24 rounded-lg">{pokemon.type2}</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-400 h-24 rounded-lg">{pokemon.height}</div>
+          <div class="flex justify-center items-center w-1/6 bg-gray-500 h-24 rounded-lg">{pokemon.weight}</div>
+        </div>
       </div>
-      <div class="flex p-2 gap-4 text-lg">
-        <div class="flex justify-center items-center w-1/6 bg-gray-400 h-24 rounded-lg">{pokemon.name}</div>
-        <img class="flex justify-center items-center w-1/6 bg-gray-500 h-24 object-cover rounded-lg" src={pokemon.image} alt=""></img>
-        <div class="flex justify-center items-center w-1/6 bg-gray-400 h-24 rounded-lg">{pokemon.type1}</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-500 h-24 rounded-lg">{pokemon.type2}</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-400 h-24 rounded-lg">{pokemon.height}</div>
-        <div class="flex justify-center items-center w-1/6 bg-gray-500 h-24 rounded-lg">{pokemon.weight}</div>
-      </div>
-    </div>
-  );
+    );
+  }
+  return;
 }
 
 export default Mon;
